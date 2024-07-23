3ff093c1d1fcaae543055d5d47447dfdc033de01
BasiliscX <juniornavarro1988@gmail.com>
2024-07-23 20:38:27 -0300

    update call for the database

--- backend/src/features/getmessages/getmessages.service.js
+++ backend/src/features/getmessages/getmessages.service.js
@@ -2,7 +2,8 @@ import prisma from '../../prisma/prismaClient.js';
 
 export const getMessagesService = async () => {
     const messages = await prisma.message.findMany({
-        orderBy: { createdAt: 'desc' }
+        orderBy: { createdAt: 'desc' },
+        select: { content: true, createdAt: true }
     });
     return messages;
 };
