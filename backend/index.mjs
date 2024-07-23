332d6f227fb48a3d8600a81e177cfb9195d53486
BasiliscX <juniornavarro1988@gmail.com>
2024-07-23 20:12:58 -0300

    added a page for te backend

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import routes from './src/index.js';
 
 dotenv.config();
@@ -10,10 +12,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use('/api', routes);
 
 app.get('/', (req, res) => {
-    res.send('Backend del Off Topic funcionando correctamente');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
