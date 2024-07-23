8ca901d32c1f937ade83083f3f5c1b1082550552
BasiliscX <juniornavarro1988@gmail.com>
2024-07-23 20:57:11 -0300

    added 'herrorhandler' middleware

--- backend/index.mjs
+++ backend/index.mjs
@@ -3,7 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import compression from 'compression';
 import routes from './src/index.js';
+import errorHandler from './src/middleware/errorHandler.js';
 
 dotenv.config();
 
@@ -11,6 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(compression());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -20,6 +23,8 @@ app.use('/api', routes);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
