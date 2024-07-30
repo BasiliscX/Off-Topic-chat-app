08481f2d1a58d30fbd595ebcfd44cc6f4af99388
BasiliscX <juniornavarro1988@gmail.com>
2024-07-30 00:15:21 -0300

    style: update Chat margin top

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 // src/App.js
 import React, { Suspense } from 'react';
 import Header from './components/Header';
-import ThemeToggle from './components/ThemeToggle';
 import Footer from './components/Footer';
 import Loader from './components/Loader';
 
@@ -9,10 +8,9 @@ const Chat = React.lazy(() => import('./features/chat/Chat'));
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen mt-10 flex flex-col">
       <Header />
       <div className="flex-grow flex flex-col items-center justify-center">
-        <ThemeToggle />
         <Suspense fallback={<Loader />}>
           <Chat />
         </Suspense>
